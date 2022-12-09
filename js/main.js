377ff4ca8fc2b024be585d8149e43e6e3d2658c2
James Rinella <rinellaj2448@gmail.com>
2022-12-09 07:51:27 -0600

    wrote out the gameplan for underline nav

--- js/main.js
+++ js/main.js
@@ -3,6 +3,19 @@ function outputCurrentYear(){
 }
 
 function underlineNav(){
-    const test = document.querySelector("#main-nav");
-    console.log(test[0]);
+    const links = document.querySelectorAll("#main-nav ul li a");
+
+    //Get title of the current page
+
+    for (let index = 0; index < links.length; index++) {
+        const element = array[index];
+        /*if(title == element.innerHtml.lower()){
+                links[index].classList.add("underline-nav");
+        }
+        else{
+            links[index].classList.remove("underline-nav");
+        }*/
+    }
 }
+
+    
