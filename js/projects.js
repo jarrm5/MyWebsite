a81e98560e15a55fae1136cccfd918e256142e77
James Rinella <rinellaj2448@gmail.com>
2022-12-11 08:41:21 -0600

    implemented the project box negative margin effect

--- js/projects.js
+++ js/projects.js
@@ -1,15 +1,13 @@
 function revealProjectDesc(){
     //console.log("mouse-over");
     this.classList.add("project-box-hover");
-    // this.children[1].classList.add("project-box-content-hover");
-    this.children[1].classList.remove("project-box-content");
     this.children[1].classList.add("project-box-content-hover");
+    
 }
 function hideProjectDesc(){
     //console.log("mouse-out");
     this.classList.remove("project-box-hover");
     this.children[1].classList.remove("project-box-content-hover");
-    this.children[1].classList.add("project-box-content");
 }
 
 const projectBoxList = document.getElementsByClassName("project-box");
