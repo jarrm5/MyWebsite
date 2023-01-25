e33d47b7ac6b42678d9e901f8738dee3d36181ea
James Rinella <rinellaj2448@gmail.com>
2023-01-25 06:22:16 -0600

    added dateRangeStr function for experience page display

--- js/experience.js
+++ js/experience.js
@@ -8,15 +8,27 @@ const experience = {
             description: "Responsible for all EDI implementations, support, troubleshooting \
             for Shamrock's Ryan Transportation brand"
         }
-    ]
+        // {
+        //     company: "Shamrock Trading Corp",
+        //     position: "EDI Specialist",
+        //     startDate: new Date(2022,8,26),
+        //     endDate: null,
+        //     description: "Responsible for all EDI implementations, support, troubleshooting \
+        //     for Shamrock's Ryan Transportation brand"
+        // }
+    ],
+    getDateRangeStr: function(index){
+        return this.jobs[index].startDate.toLocaleDateString('en-us', { year:"numeric", month:"long"}) +
+            (experience.jobs[index].endDate === null ? " - Present" : " - " + experience.jobs[index].endDate.toLocaleDateString('en-us', { year:"numeric", month:"long"}));
+    }
 }
 
 const boxes = document.getElementsByClassName("exp-box");
-for (let index = 0; index < boxes.length; index++) {
-    boxes[index].children[0].innerHTML = experience.jobs[index].company;
-    boxes[index].children[2].innerHTML = experience.jobs[index].position;
-    let dateStr = experience.jobs[index].startDate.toLocaleDateString('en-us', { year:"numeric", month:"long"});
-    dateStr += (experience.jobs[index].endDate === null ? " - Present" : " - " + experience.jobs[index].endDate.toLocaleDateString('en-us', { year:"numeric", month:"long"}));
-    boxes[index].children[3].innerHTML = dateStr;
-    boxes[index].children[4].children[0].innerHTML = experience.jobs[index].description;
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].children[0].innerHTML = experience.jobs[i].company;
+    boxes[i].children[2].innerHTML = experience.jobs[i].position;
+    boxes[i].children[3].innerHTML = experience.getDateRangeStr(i);
+    for (let j = 0; j < boxes[i].children[4].children.length; j++) {
+        boxes[i].children[4].children[j].innerHTML = experience.jobs[i].description;
+    }
 }
