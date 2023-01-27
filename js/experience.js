e6e529878bde3226f24de642ded320951b2f40fd
James Rinella <rinellaj2448@gmail.com>
2023-01-27 06:50:10 -0600

    improved experience object but broke the website

--- js/experience.js
+++ js/experience.js
@@ -2,20 +2,55 @@ const experience = {
     jobs : [
         {
             company: "Shamrock Trading Corp",
-            position: "EDI Specialist",
-            startDate: new Date(2022,8,26),
-            endDate: null,
-            description: "Responsible for all EDI implementations, support, troubleshooting \
-            for Shamrock's Ryan Transportation brand"
+            positions: [
+                {
+                    title: "EDI Specialist",
+                    startDate: new Date(2022,8,26),
+                    endDate: null,
+                    description: [
+                        {
+                            text:  "Responsible for all EDI implementations, support, troubleshooting \
+                            for Shamrock's Ryan Transportation brand"
+                        }
+                    ]
+                }
+            ]
+            
+        },
+        {
+            company: "Stord",
+            positions: [
+                {
+                    title: "Integration Engineer",
+                    startDate: new Date(2022,4,11),
+                    endDate: new Date(2022,8,22),
+                    descriptions: [
+                        {
+                            text: "Designed and implemented core API process flows integrated with \
+                            the our main warehouse management systems, accomodating all \
+                            company owned warehouses and their 30+ integrated customers."
+                        },
+                        {
+                            text: "Headed the integration effort to migrate 75+ customers from legacy \
+                            warehouse management system onto the Stord platform after an \
+                            acquisition of a smaller logistics firm using the above processes."
+                        }  
+                    ]
+                },
+                {
+                    title: "Integration Specialist",
+                    startDate: new Date(2021,2,16),
+                    endDate: new Date(2022,4,11),
+                    description: [{
+                        text: "Onboarded several clients during phases of project management, \
+                        technical development, utilizing API connections, json, xml, EDI \
+                        data"
+                        }
+                    ]
+                }
+            ]
+            
         }
-        // {
-        //     company: "Shamrock Trading Corp",
-        //     position: "EDI Specialist",
-        //     startDate: new Date(2022,8,26),
-        //     endDate: null,
-        //     description: "Responsible for all EDI implementations, support, troubleshooting \
-        //     for Shamrock's Ryan Transportation brand"
-        // }
     ],
     getDateRangeStr: function(index){
         return this.jobs[index].startDate.toLocaleDateString('en-us', { year:"numeric", month:"long"}) +
