511095e961b410405597c04a58fe48b498cbe21b
James Rinella <rinellaj2448@gmail.com>
2023-01-27 20:53:35 -0600

    attempted to fix experience object looping

--- js/experience.js
+++ js/experience.js
@@ -24,7 +24,7 @@ const experience = {
                     title: "Integration Engineer",
                     startDate: new Date(2022,4,11),
                     endDate: new Date(2022,8,22),
-                    descriptions: [
+                    description: [
                         {
                             text: "Designed and implemented core API process flows integrated with \
                             the our main warehouse management systems, accomodating all \
@@ -41,29 +41,36 @@ const experience = {
                     title: "Integration Specialist",
                     startDate: new Date(2021,2,16),
                     endDate: new Date(2022,4,11),
-                    description: [{
-                        text: "Onboarded several clients during phases of project management, \
-                        technical development, utilizing API connections, json, xml, EDI \
-                        data"
+                    description: [
+                        {
+                            text: "Onboarded several clients during phases of project management, \
+                            technical development, utilizing API connections, json, xml, EDI \
+                            data"
                         }
                     ]
                 }
-            ]
-            
+            ]   
         }
     ],
-    getDateRangeStr: function(index){
-        return this.jobs[index].startDate.toLocaleDateString('en-us', { year:"numeric", month:"long"}) +
-            (experience.jobs[index].endDate === null ? " - Present" : " - " + experience.jobs[index].endDate.toLocaleDateString('en-us', { year:"numeric", month:"long"}));
+    getDateRangeStr: function(jobIndex,positionIndex){
+        return this.jobs[jobIndex].positions[positionIndex].startDate.toLocaleDateString('en-us', { year:"numeric", month:"long"}) +
+            (experience.jobs[jobIndex].positions[positionIndex].endDate === null ? " - Present" : " - " + experience.jobs[jobIndex].positions[positionIndex].endDate.toLocaleDateString('en-us', { year:"numeric", month:"long"}));
     }
 }
 
 const boxes = document.getElementsByClassName("exp-box");
+const descList = document.getElementsByClassName("exp-box-desc");
+
 for (let i = 0; i < boxes.length; i++) {
     boxes[i].children[0].innerHTML = experience.jobs[i].company;
-    boxes[i].children[2].innerHTML = experience.jobs[i].position;
-    boxes[i].children[3].innerHTML = experience.getDateRangeStr(i);
-    for (let j = 0; j < boxes[i].children[4].children.length; j++) {
-        boxes[i].children[4].children[j].innerHTML = experience.jobs[i].description;
+    for (let j = 0; j < experience.jobs[i].positions[j].length; j++) {
+        boxes[i].children[2].innerHTML = experience.jobs[i].title;
+        boxes[i].children[3].innerHTML = experience.getDateRangeStr(i,j);
+        for (let k = 0; k < experience.jobs[i].positions[j].description[k].length; k++) {
+            const node = document.createElement("li");
+            node.appendChild(document.createTextNode(experience.jobs[i].positions[j].description[k].text));
+            boxes[i].children[4].children[j].innerHTML = experience.jobs[i].positions[j].description[k].text;
+            descList[i].appendChild(node);
+        }
     }
 }
